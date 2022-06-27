9d71e91981951293e0d0b7abf13c4ebefcade3a4
ApeMachine <max.dueppe@gmx.net>
2022-06-27 14:19:38 +0200

    Commit Nr. 3

--- File1.java
+++ File1.java
@@ -1,5 +1,7 @@
 public class File1{
 	public static void main(String[] args){
+		String text = "Lalelu nur der Mann im Mond schaut zu...";
 		System.out.println("Grüße, ich bin File1!");
+		System.out.println(text + "!");
 	}
 }
