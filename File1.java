073d48edea7c7b341139677fdb434ef02d7d02c2
ApeMachine <max.dueppe@gmx.net>
2022-06-27 14:37:02 +0200

    bla

--- File1.java
+++ File1.java
@@ -2,6 +2,6 @@ public class File1{
 	public static void main(String[] args){
 		String text = "Lalelu nur der Mann im Mond schaut zu...";
 		System.out.println("Grüße, ich bin File1!");
-		System.out.println(text + "!");
+	//	System.out.println(text + "!");
 	}
 }
